accc032c2a412e708bee30ef1b7c4b45d6ecd6fd
Asaoka Takuya <dimas080201@gmail.com>
2023-06-04 04:02:33 +0900

    modify

--- knexfile.js
+++ knexfile.js
@@ -1,25 +1,24 @@
-
-require("dotenv").config({
-    path: ".env",
-})
+require('dotenv').config({
+  path: '.env',
+});
 module.exports = {
-    development: {
-        client: "pg",
-        connection: {
-            user: process.env.DB_USER,
-            database: process.env.DB_NAME,
-        },
-        migrations: {
-            directory: "./db/migrations",
-        },
-        seeds: { directory: "./db/seeds" },
+  development: {
+    client: 'pg',
+    connection: {
+      user: process.env.DB_USER,
+      database: process.env.DB_NAME,
     },
-    production: {
-        client: "pg",
-        connection: process.env.DATABASE_URL,
-        migrations: {
-            directory: "./db/migrations",
-        },
-        seeds: { directory: "./db/seeds" },
-    }
+    migrations: {
+      directory: './db/migrations',
+    },
+    seeds: { directory: './db/seeds' },
+  },
+  production: {
+    client: 'pg',
+    connection: process.env.DB_URL,
+    migrations: {
+      directory: './db/migrations',
+    },
+    seeds: { directory: './db/seeds' },
+  },
 };

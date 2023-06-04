968619f9134f294a2ce769528d00ebd09146a65b
Asaoka Takuya <dimas080201@gmail.com>
2023-06-04 14:57:21 +0900

    modify

--- src/App.jsx
+++ src/App.jsx
@@ -9,11 +9,12 @@ import { UserRegistration } from './components/UserRegistration';
 import { production } from '../knexfile';
 
 function App() {
+  const fetchServer = 'https://sumgcampserver.onrender.com';
   // const fetchServer = process.env.SERVER_URL ?? 'http://localhost:8080';
-  const fetchServer =
-    process.env.NODE_ENV === 'production'
-      ? 'https://sumgcampserver.onrender.com'
-      : 'http://localhost:8080';
+  // const fetchServer =
+  //   process.env.NODE_ENV === 'production'
+  //     ? 'https://sumgcampserver.onrender.com'
+  //     : 'http://localhost:8080';
 
   const [view, setView] = useState('ItemList');
   const pageChange = (name) => setView(name);
